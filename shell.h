09a78f2d53e5b33e3c7d590ce595605a138df685
Arwa Magdi <arwamagdi20@gmail.com>
2023-05-23 22:59:22 +0300

    update

--- shell.h
+++ shell.h
@@ -1,17 +1,7 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <linux/limits.h>
+
 
 /* Macros */
 #define PERMISSION_ERR 126
