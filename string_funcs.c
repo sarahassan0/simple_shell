703bcf642e9743bd79fa7c9772db10699230939c
Arwa Magdi <arwamagdi20@gmail.com>
2023-05-24 21:35:18 +0300

    m

--- string_funcs.c
+++ string_funcs.c
@@ -131,39 +131,7 @@ char *num_to_char(int num)
 	return (str_num);
 }
 
-/**
- * _atoi - covert string to number.
- * @s: string to be converted to number.
- *
- * Return: result of converting string to number, -1 on failure.
- */
 
-int _atoi(char *s)
-{
-	unsigned int result, i;
-	char sign;
-
-	i = 0;
-	result = 0;
-
-	while (s[i] != '\0')
-	{
-		if (!((s[i] >= '0') && (s[i] <= '9')))
-		{
-			return (-1);
-		}
-		if (((s[i] >= '0') && (s[i] <= '9')))
-		{
-			result = (result * 10) + (s[i] - '0');
-		}
-		else if (s[i] == '+')
-			sign++;
-
-		i++;
-	}
-
-	return (result);
-}
 
 /**
  * free_arr - Free the memory allocated for an array of strings.
